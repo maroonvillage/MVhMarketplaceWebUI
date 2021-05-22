8afd00cb9dc99cedfb479e5ddfadc83ca97d7995
Cole Parsons <coleparsons@hotmail.com>
2021-05-22 02:23:18 -0700

    * SiteLink.cs: * BlockLink.cs: * SiteLogo.cshtml: * HeaderImage.cshtml:
    * webui-Debug.json: Added files to support dynamic links and images

---
+++ Models/BlockLink.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace webui.Models
+{
+    public class BlockLink
+    {
+        public BlockLink()
+        {
+        }
+
+        public int BlockLinkId { get; set; }
+        public int BlockId { get; set; }
+        public int LinkId { get; set; }
+
+    }
+}
---
+++ Models/SiteLink.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace webui.Models
+{
+    public class SiteLink
+    {
+        public SiteLink()
+        {
+        }
+
+        public int? LinkId { get; set; }
+        public string LinkName { get; set; }
+        public string Url { get; set; }
+        public string ToolTip { get; set; }
+        public string Title { get; set; }
+        public string Target { get; set; }
+        public int WebsiteId { get; set; }
+        public BlockLink BlockLink { get; set; }
+        public SiteImage SiteImage { get; set; }
+
+    }
+}
