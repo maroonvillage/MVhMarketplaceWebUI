c12b110d02e6890c9e195b4b46dfad0f47fd34ac
Cole Parsons <coleparsons@hotmail.com>
2021-12-14 22:25:47 -0800

    Added new interface to support comos db querying

--- Interfaces/ICosmosDbWebContent.cs
+++ Interfaces/ICosmosDbWebContent.cs
@@ -1,7 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 namespace webui.Interfaces
 {
     public interface ICosmosDbWebContent
     {
+        Task GetSitePagesByMarketplaceIdAsync(string marketId);
+        Task etSitePagesByPageMachineNameAsync(string marketName);
+        Task GetMenusByMarketplaceIdAsync(string marketId);
+        Task GetMenusByNameAsync(string menuName);
+        Task GetBlockImagesByBlockIdAsync(string blockId);
+        Task GetBlockLinksByBlockIdAsync(string blockId);
     }
 }
